e14978a518bbad0f25659a1267162ef9288432ca
Midhun Menon <midhun@2basetechnologies.com>
2026-04-20 23:37:41 +0530

    changes in main.jsx&package.json

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ClaudeLearningPlan2BaseFirebase from "./firebase-app.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ClaudeLearningPlan2BaseFirebase />
+  </React.StrictMode>
+);
